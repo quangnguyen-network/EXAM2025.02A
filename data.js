66b111bd6c90e6291c0180935c1e3bef676ace73
Nguyen Bao Quang <baoquangnguyen104@gmail.com>
2026-06-15 09:42:13 +0700

    Initial commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const matHangData = [
+  { id: 1, productName: "Pickleball Pro Elite", category: "Pickleball Paddle", price: "2500000", stock: "15", description:"Professional racquet for tournament players" },
+  { id: 2, productName: "Pickleball Tournament X6", category: "Pickleball Ball", price: "450000", stock: "50", description:"Set of 6 high quality balls for competition" },
+  { id: 3, productName: "Deluxe Racquet Bag", category: "Bag", price: "850000", stock: "8", description:"Premium racket bag with multiple compartments" },
+  { id: 4, productName: "Pickleball Sports Shirt", category: "Apparel", price: "320000", stock: "25", description:"Cool, sweat-absorbent sports shirt" },
+  { id: 5, productName: "Giày Pickleball Court Master", category: "Shoes", price: "1800000", stock: "0", description:"Specialized shoes with anti-slip soles" }
+];
